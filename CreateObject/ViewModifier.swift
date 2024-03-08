ca237e404862fda47599f94e9f9b19db1030551a
BrianBAbraham <brianbabraham@gmail.com>
2024-03-08 18:52:14 +0000

    more error sorting in menu

---
+++ CreateObject/ViewModifier.swift
@@ -0,0 +1,18 @@
+//
+//  ViewModifier.swift
+//  CreateObject
+//
+//  Created by Brian Abraham on 08/03/2024.
+//
+
+import SwiftUI
+
+struct ViewModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewModifier()
+}
