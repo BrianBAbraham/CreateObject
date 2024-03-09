bf4dafb5c21d50f16d1ed34f5dc082853e0c402a
BrianBAbraham <brianbabraham@gmail.com>
2024-03-09 16:36:24 +0000

    worki on menus

---
+++ CreateObject/BilateralPartEdit.swift
@@ -0,0 +1,18 @@
+//
+//  BilateralPartEdit.swift
+//  CreateObject
+//
+//  Created by Brian Abraham on 09/03/2024.
+//
+
+import SwiftUI
+
+struct BilateralPartEdit: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BilateralPartEdit()
+}
