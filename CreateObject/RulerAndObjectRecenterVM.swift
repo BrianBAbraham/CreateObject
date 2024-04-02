8815bdd973f3d83dabf62d8c6e2aa6e34f654442
BrianBAbraham <brianbabraham@gmail.com>
2024-04-02 18:44:25 +0100

    work done on ruler

---
+++ CreateObject/RulerAndObjectRecenterVM.swift
@@ -0,0 +1,14 @@
+//
+//  RulerAndObjectRecenterVM.swift
+//  CreateObject
+//
+//  Created by Brian Abraham on 02/04/2024.
+//
+
+import Foundation
+
+
+class Recenter: ObservableObject {
+    @Published var recentre = Screen.height / 2.0
+    
+}
