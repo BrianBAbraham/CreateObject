ce66efbc9ab5de2b0266ff2d32e99e36e827308d
BrianBAbraham <brianbabraham@gmail.com>
2024-04-13 19:30:46 +0100

    the movement is now working as a sperate view

---
+++ CreateObject/MovementPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  MovementPickerView.swift
+//  CreateObject
+//
+//  Created by Brian Abraham on 13/04/2024.
+//
+
+import SwiftUI
+
+struct MovementPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MovementPickerView()
+}
